7f5cb4affcf8104cbc2e77de4b2eeb904f8aec82
PauloZupper <paulo.fernandes@zup.com.br>
2024-03-07 08:33:57 -0300

    fix build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,79 +1,18 @@
-{
-  "root": true,
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jsx-a11y/recommended"
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  "overrides": [],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
-  "plugins": [
-    "react",
-    "react-hooks"
-  ],
-  "rules": {
-    "@typescript-eslint/no-namespace": "off",
-    "jsx-a11y/aria-role": [
-      2,
-      {
-        "ignoreNonDOM": true
-      }
-    ],
-    "no-extra-boolean-cast": "off",
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/display-name": "off",
-    "react/jsx-key": "off",
-    "import/order": [
-      2,
-      {
-        "groups": [
-          "external",
-          "builtin",
-          "internal",
-          "parent",
-          "sibling",
-          "index"
-        ]
-      }
-    ]
-  },
-  "settings": {
-    "react": {
-      "version": "detect"
-    },
-    "import/core-modules": [
-      "miragejs/-types"
-    ],
-    "import/ignore": [
-      "react-apexcharts"
-    ],
-    "import/resolver": {
-      "typescript": true,
-      "node": {
-        "paths": [
-          "src",
-        ],
-        "extensions": [
-          ".js",
-          ".jsx",
-          ".ts",
-          ".tsx"
-        ]
-      }
-    }
-  }
 }
