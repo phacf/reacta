14019b431d48b0e974165f75479d8b6c23ba90d6
PauloZupper <paulo.fernandes@zup.com.br>
2024-03-12 11:08:34 -0300

    test iframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Screen } from "./Containers";
+import {Page, Screen } from "./Containers";
 
 import './index.css';
 
@@ -8,6 +8,10 @@ function App() {
   return (
     <div className="bg-gradient-to-r  w-screen h-screen from-primary-700 flex justify-center items-center to-primary-650">
       <Screen>
+        <Page>
+
+        <iframe src="https://medium.com/@paulo.fernandes_27955/teste-fbc5adea3905" />
+        </Page>
        </Screen>
     </div>
   );
--- src/Containers/Page/Page.tsx
+++ src/Containers/Page/Page.tsx
@@ -7,18 +7,20 @@ interface PageProps extends DivType {
 }
 
 export const Page = (props: PageProps) => {
+  const { onClose, label = 'Page'} = props
   return (
     <div className="w-full h-full bg-primary-400 relative">
       <div className="w-full z-10 h-full border-t-2 border-primary-200 absolute top-0 left-0  "></div>
       <div className="w-full z-10 h-full border-r-2 border-primary-900 absolute top-0 right-0 "></div>
 
-      <div className="w-[99%] top-1 py-1 px-2 z-50 bg-primary-600 left-1 absolute  text-primary-200 flex items-center justify-between">
-        {props.label}
-        <CloseButton onClick={props.onClose} >X</CloseButton>
+      <div className="w-[83.4rem] top-1 py-1 px-2 z-50 bg-primary-600 left-1 absolute  text-primary-200 flex items-center justify-between">
+        {label}
+        <CloseButton onClick={onClose} >X</CloseButton>
       </div>
       <div className="w-full h-full border-b-2 border-primary-900 absolute bottom-0 left-0 "></div>
       <div className="w-full h-full border-l-2 border-primary-200 absolute bottom-0 right-0 "></div>
-      <div className='flex'>
+      <div className='flex pt-16 pl-1 pr-1 pb-1'>
+
         {props.children}
       </div>
     </div>
